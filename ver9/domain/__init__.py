70553f3eeeb2218cef4850a95665336e3bcc51ef
Nims-S <nimsts@gmail.com>
2026-05-22 00:34:56 +0530

    Create pure domain package

---
+++ ver9/domain/__init__.py
@@ -0,0 +1,17 @@
+"""Pure immutable domain layer.
+
+This package contains:
+- immutable event contracts
+- immutable execution models
+- enums
+- identifiers
+- state snapshots
+
+The domain layer must NEVER import:
+- runtime
+- exchanges
+- OMS
+- persistence
+- logging
+- metrics
+"""
